fc29cab0a950df95f9dec742befb3d4f2e1945a1
Alena <elenila_skinef@mail.ru>
2022-02-07 23:36:56 +0300

    programed the bot to count from 0 to any positive number users enter.

--- script.js
+++ script.js
@@ -12,9 +12,19 @@ console.log(`What a great name you have, ${yourName}!`);
 console.log("Let me guess your age.");
 console.log("Enter remainders of dividing your age by 3, 5 and 7.");
 
-let remainder3 = input();
-let remainder5 = input();
-let remainder7 = input();
+let remainder3 = Number(input());
+let remainder5 = Number(input());
+let remainder7 = Number(input());
+
 let yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
 
 console.log(`Your age is ${yourAge}; that's a good time to start programming!`);
+console.log("Now I will prove to you that I can count to any number you want.");
+
+let yourNumber = Number(input());
+
+for (let number = 0; number <= yourNumber; number++) {
+  console.log(`${number}!`);  
+}
+
+console.log("Completed, have a nice day!");
