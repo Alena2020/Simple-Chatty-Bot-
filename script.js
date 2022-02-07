fbbd6b08af4aa5f72ea3837591e0271d5016530e
Alena <elenila_skinef@mail.ru>
2022-02-07 21:36:14 +0300

    created a dialogue with the bot

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("Hello! My name is Orion.");
-console.log("I was created in 2022.");
+const input = require('sync-input');
+let botName = "Orion";
+let birthYear = 2022;
+let yourName = input();
+
+console.log(`Hello! My name is ${botName}.`);
+console.log(`I was created in ${birthYear}.`);
+console.log("Please, remind me your name.");
+console.log(`What a great name you have, ${yourName}!`);
