be5d79f9239bdb47792104aa4db64cb8685ef2c5
Alena <elenila_skinef@mail.ru>
2022-02-07 22:28:56 +0300

    The bot try to guess your age using arithmetic operations

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const input = require('sync-input');
 let botName = "Orion";
 let birthYear = 2022;
-let yourName = input();
 
 console.log(`Hello! My name is ${botName}.`);
 console.log(`I was created in ${birthYear}.`);
 console.log("Please, remind me your name.");
+
+let yourName = input();
+
 console.log(`What a great name you have, ${yourName}!`);
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+let remainder3 = input();
+let remainder5 = input();
+let remainder7 = input();
+let yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+console.log(`Your age is ${yourAge}; that's a good time to start programming!`);
