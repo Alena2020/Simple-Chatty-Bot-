48746dd5b7f9d769232172d5109c34b32a898868
Alena <elenila_skinef@mail.ru>
2022-02-10 22:23:36 +0300

    created interface for bot

--- script.js
+++ script.js
@@ -4,11 +4,8 @@ let botName = "Orion";
 let birthYear = 2022;
 
 function greet(botName, birthYear) {
-  let string1 = `Hello! My name is ${botName}.`;
-  let string2 = `I was created in ${birthYear}.`;
-
-  showDialogueWindow(string1);
-  showDialogueWindow(string2);
+  showDialogueWindow(`Hello! My name is ${botName}.`);
+  showDialogueWindow(`I was created in ${birthYear}.`);
 }
 
 function remindName() {
@@ -16,16 +13,14 @@ function remindName() {
   console.log(string1);
   dialogueWindow.innerHTML = `🤖  ${string1}`;
   let yourName = prompt(string1, "");
-  let string2 = `What a great name you have, ${yourName}!`;
 
   // let yourName = input();
 
-  showDialogueWindow(string2);
+  showDialogueWindow(`What a great name you have, ${yourName}!`);
 }
 
 function guessAge() {
-  let string1 = "Let me guess your age.";
-  showDialogueWindow(string1);
+  showDialogueWindow("Let me guess your age.");
 
   let string2 = "Enter remainders of dividing your age by 3, 5 and 7.";
   console.log(string2);
@@ -40,8 +35,7 @@ function guessAge() {
 
   let yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
 
-  let string3 = `Your age is ${yourAge}; that's a good time to start programming!`;
-  showDialogueWindow(string3);
+  showDialogueWindow(`Your age is ${yourAge}; that's a good time to start programming!`);
 }
 
 function count() {
@@ -49,7 +43,6 @@ function count() {
   console.log(string1);
   dialogueWindow.innerHTML = `🤖  ${string1}`;
   
-
   // let yourNumber = Number(input());
   let yourNumber = prompt(string1, "");
 
@@ -59,34 +52,27 @@ function count() {
 }
 
 function test() {
-  let string1 = "Let's test your programming knowledge.";
   let question = `Why do we use methods?
     1. To repeat a statement multiple times.
     2. To decompose a program into several small subroutines.
     3. To determine the execution time of a program.
     4. To interrupt the execution of a program.`;
 
-  showDialogueWindow(string1);  
-
-  while (true) {
-    console.log(question);
-    dialogueWindow.innerHTML = `🤖  ${question}`;
-		let answer = prompt(question, "");
+  showDialogueWindow("Let's test your programming knowledge."); 
+  
+  console.log(question);
+  dialogueWindow.innerHTML = `🤖  ${question}`;
+	let answer = prompt(question, "");
     
-		if (answer === "2") {
-      let completed = "Completed, have a nice day!";
-      showDialogueWindow(completed);
-			break;
-		} else {
-      let tryAgain = "Please, try again.";
-      showDialogueWindow(tryAgain);
-		}
+	while (answer !== "2") {
+      showDialogueWindow("Please, try again.");
+      answer = prompt(question, "");
 	}
+  showDialogueWindow("Completed, have a nice day!");	
 }
   
 function end() {
-  let congratulations = "Congratulations, have a nice day!";
-  showDialogueWindow(congratulations);
+  showDialogueWindow("Congratulations, have a nice day!");
 }
 
 function showDialogueWindow(string) {
